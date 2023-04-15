90d3f715c1e729d60dcde811581c63d6744a0cac
AkilaWic <forsepwicki92@gmail.com>
2023-04-15 14:57:08 +0530

    Developed category entry activities

---
+++ app/src/main/java/com/example/mindfulness/Adhd.java
@@ -0,0 +1,15 @@
+package com.example.mindfulness;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Adhd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adhd_enty_page2);
+
+    }
+}
