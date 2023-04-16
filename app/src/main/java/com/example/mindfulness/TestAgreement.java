e571becc90b857d29b3ee94b716f029f46db5f91
AkilaWic <forsepwicki92@gmail.com>
2023-04-16 22:43:55 +0530

    Stress UI updated

--- app/src/main/java/com/example/mindfulness/TestAgreement.java
+++ app/src/main/java/com/example/mindfulness/TestAgreement.java
@@ -1,6 +1,9 @@
 package com.example.mindfulness;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,16 @@ public class TestAgreement extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_agreement);
 
+        //Test Instruction
+        Button ti = (Button) findViewById(R.id.agree);
+
+        ti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create new intent
+                Intent ti = new Intent(TestAgreement.this, TestInstructions.class);
+                startActivity(ti);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mindfulness/TestInstructions.java
@@ -0,0 +1,14 @@
+package com.example.mindfulness;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestInstructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_instructions);
+    }
+}
