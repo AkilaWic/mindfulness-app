4b50987f3e8cc301ee50721404e5d84d75dee361
AkilaWic <forsepwicki92@gmail.com>
2023-04-18 23:31:33 +0530

    Added About Depression. (AboutDepression.java, about_depression.xml)

--- app/src/main/java/com/example/mindfulness/Depression.java
+++ app/src/main/java/com/example/mindfulness/Depression.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +24,18 @@ public class Depression extends AppCompatActivity {
                 //Create new intent
                 Intent taketest = new Intent(Depression.this, TestAgreement.class);
                 startActivity(taketest);
+            }
+        });
+
+        //About Depression
+        TextView about = (TextView) findViewById(R.id.about);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create new intent
+                Intent about = new Intent(Depression.this, AboutDepression.class);
+                startActivity(about);
             }
         });
     }
